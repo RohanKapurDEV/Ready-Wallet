60d4a393d7c296914a2f7369fefae47c813c6078
RohanKapurDEV <rohankapurdev@outlook.com>
2019-02-27 05:36:56 -0500

    Made splash and landing page and began modifying landing page markup to emulate desired layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'splash', loadChildren: './pages/splash/splash.module#SplashPageModule' },
+  { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },
 ];
 
 @NgModule({
