b9590b264e549a5105640b8394f6619805bf08a1
RohanKapurDEV <rohankapurdev@outlook.com>
2019-02-27 08:07:24 -0500

    Added pricing pulls, icons, and spacing. Gonna tackle charts next

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NomicsService } from '../../services/nomics.service'
+import * as $ from 'jquery';
+var numeral = require('numeral');
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPage implements OnInit {
 
-  constructor() { }
+  btcPrice: any;
+  ethPrice: string;
+
+  constructor(private nomics: NomicsService) { }
 
   ngOnInit() {
+    this.pullBTCPriceFromNomics()
+    this.pullETHPriceFromNomics()
+  }
+
+  pullBTCPriceFromNomics() {
+    this.nomics.getPriceBySymbol('BTC').subscribe(
+      (result) => {this.btcPrice = numeral(result[0].price).format('$ 0,0.00')}
+    )
+  }
+  
+  pullETHPriceFromNomics() {
+    this.nomics.getPriceBySymbol('ETH').subscribe(
+      (result) => {this.ethPrice = numeral(result[0].price).format('$ 0,0.00')}
+    )
   }
 
 }
--- src/app/services/nomics.service.ts
+++ src/app/services/nomics.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { tap, map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class NomicsService {
 
-  constructor() { }
+  apiUrl: string = 'https://api.nomics.com/v1';
+  apiKey: string = '0e9a6df5db1b0d1f8c5d5d6c182019ae';
+
+  constructor(private http: HttpClient) { }
+
+  getPriceBySymbol(sym: string) {
+    // Sym input has to be of the following format - BTC, ETH, LTC, ZEC, etc ... 
+    return this.http.get(`${this.apiUrl}/prices?key=${this.apiKey}`).pipe(
+      tap(console.log),
+      map(array => array.filter(obj => obj.currency === sym))
+    )
+  }
 }
